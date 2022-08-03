cdfb3ba0019168fa265b2b72abce9f1b468931e9
Jessica Ray <jray@csail.mit.edu>
2022-08-03 17:04:55 -0400

    update cola jpeg script for laptop

--- apps/eval/scripts/laptop/cola_jpeg_baseline.sh
+++ apps/eval/scripts/laptop/cola_jpeg_baseline.sh
@@ -16,7 +16,7 @@ git log -1 &>$dir/commit
 
 # compile
 cd $bindir
-$cola/build/colac build -release -cfg $cola/apps/eval/scripts/laptop/jpeg.cfg jpeg.seq
+$cola/build/colac build -release jpeg.seq
 
 ##### ADD BENCHMARK HERE
 rm -f $dir/flower.log
@@ -46,6 +46,8 @@ done
 # decode it and print the psnr
 cp /tmp/cola.jpg $dir/flower.jpg
 md5 $dir/flower.jpg >> $dir/flower.log
+echo "Flower md5"
+echo md5 $dir/flower.jpg
 
 # spider 
 cmd="$bindir/jpeg $spider"
@@ -63,6 +65,8 @@ done
 # decode it and print the psnr
 cp /tmp/cola.jpg $dir/spider.jpg
 md5 $dir/spider.jpg >> $dir/spider.log
+echo "Spider md5"
+echo md5 $dir/spider.jpg
 
 # cathedral 
 cmd="$bindir/jpeg $cathedral"
@@ -80,3 +84,5 @@ done
 # decode it and print the psnr
 cp /tmp/cola.jpg $dir/cathedral.jpg
 md5 $dir/cathedral.jpg >> $dir/cathedral.log
+echo "Cathedral md5"
+echo md5 $dir/cathedral.jpg
